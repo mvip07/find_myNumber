7dd78ce67c718e8bd4f3915d81829dba59b1e673
Ozodov Mirafzal <90509996+mvip07@users.noreply.github.com>
2024-02-14 12:47:34 +0500

    Update main.js

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,151 +1,59 @@
-let big_List = []
-let small_List = [...big_List]
 let count = 1
+let big_List = []
 
+const body = document.querySelector("body")
 const modal = document.querySelector(".modal")
-const found = document.querySelector(".put__found")
-const container = document.querySelector(".container")
-const select = document.querySelector(".game__choose__number")
+const select = document.querySelector("#select")
+const found = document.querySelector(".put_found")
+const get_blur = document.querySelector(".get_blur")
+const addNumber = document.querySelector(".add_number")
 
 choose__number()
 
-function btn1() {
-
-    if (count < 3) {
-
-        let array = []
-
-        sort__list(1, array)
-
-        sort__list(0, array)
-
-        sort__list(2, array)
-
-        big_List = array
-
-        list__append(array)
-
-        count += 1
-
-    } else add__modal(0)
-}
-
-function btn2() {
-
-    if (count < 3) {
-
-        let array = []
-
-        sort__list(2, array)
-
-        sort__list(1, array)
-
-        sort__list(0, array)
-
-        big_List = array
-
-        list__append(array)
-
-        count += 1
-
-    } else add__modal(1)
-
-}
-
-function btn3() {
-
-    if (count < 3) {
-
-        let array = []
-
-        sort__list(1, array)
-
-        sort__list(2, array)
-
-        sort__list(0, array)
-
-        big_List = array
-
-        list__append(array)
-
-        count += 1
-
-    } else add__modal(2)
-
-}
-
-function replay() {
-    count = 1
-    select.value = "Choose"
-    list__append(small_List)
-    modal.classList.remove("active")
-    container.classList.remove("active")
-}
-
-function add__modal(num) {
-    let array = []
-    switch (num) {
-        case 0:
-            sort__list(0, array)
-            break
-        case 1:
-            sort__list(1, array)
-            break
-        case 2:
-            sort__list(2, array)
-    }
-
-    modal.classList.add("active")
-    container.classList.add("active")
-    found.innerHTML = array[parseInt(array.length / 2)]
-}
-
 function choose__number() {
-    select.addEventListener("change", ({ target }) => {
-        switch (target.value) {
-            case "15":
-                push__number__list(15)
-                break
-            case "21":
-                push__number__list(21)
-                break
-            case "27":
-                push__number__list(27)
-                break
-            default:
-                alert("Selectdan Tanlang")
-                break
-        }
-    })   
-}
-
-function list__append(list) {
-    const body = document.querySelector(".container__body")
-    body.innerHTML = ""
-    for (let i = 0; i < list.length; i++) {
-        let div = document.createElement("div")
-        div.innerHTML = list[i]
-        body.append(div)
-    }
+    select.addEventListener("change", ({ target }) => push__number__list(Number(target.value)))
+    push__number__list(15)
 }
 
 function push__number__list(num) {
     big_List = []
-    let defined = num
-
-    while (defined > 0) {
+    while (num > 0) {
         let random = Math.floor(Math.random() * 99);
-        if (big_List.includes(random) == false) {
-            big_List.push(random)
-            defined -= 1
+        if (!big_List.includes(random)) {
+            big_List.push(random); num -= 1
         }
     }
+    list__append()
+}
 
-    list__append(big_List)
+function list__append() {
+    addNumber.innerHTML = ""
+    for (let i of big_List) addNumber.innerHTML += `<div class="number">${i}</div>`
 }
 
 function sort__list(num, gap__list) {
-    for (let i = num; i < big_List.length; i += 3) {
-        gap__list.push(big_List[i])
-    }
+    for (let i = num; i < big_List.length; i += 3) gap__list.push(big_List[i])
 }
+
+function add__modal(num) {
+    let array = []
+    sort__list(num, array)
+    body.classList.add("hidden")
+    modal.classList.add("active")
+    get_blur.classList.add("blur")
+    found.innerHTML = array[parseInt(array.length / 2)]
+}
+
+function btn_click(num1, num2, num3, addNumber) {
+    if (count < 3) {
+        let array = []
+        sort__list(num1, array)
+        sort__list(num2, array)
+        sort__list(num3, array)
+        big_List = array
+        list__append()
+        count += 1
+    } else add__modal(addNumber)
+}
+
+const replay = () => location.reload()
